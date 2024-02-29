087c1dd50bd76e3bddd87eef8b1ad350aaef06b0
AbooSalh <a.m0507979@gmail.com>
2024-02-29 12:06:06 +0200

    navigation bullets

--- main.js
+++ main.js
@@ -237,3 +237,13 @@ document.addEventListener("click", (e) => {
     toggleGear();
   }
 });
+
+// seclect all bullets
+const allBullets = document.querySelectorAll(".nav-bullets .bullet");
+allBullets.forEach((bullet) => {
+  bullet.addEventListener("click", (e) => {
+    document.querySelector(e.target.dataset.section).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
